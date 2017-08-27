10c09c7abc3e10e1e6a65dc62123d7ce65c69b90
wangsongiam <wangsongiam@gmail.com>
2017-08-26 23:23:41 -0400

    basic functionality

---
+++ server/db/index.js
@@ -0,0 +1,16 @@
+import Sequelize from 'sequelize'
+
+const sequelize = new Sequelize('graphqlpost', 'graphql_tester', 'j', {
+  host: 'localhost',
+  dialect: 'postgres'
+})
+
+
+const db = {
+  User: sequelize.import('./user')
+}
+
+
+db.sequelize = sequelize
+
+export default db
